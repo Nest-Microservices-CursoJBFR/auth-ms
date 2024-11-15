86ba2e8b60741d1b2d12b7d511da734860ed7246
locobenjas <103694742+locobenjas@users.noreply.github.com>
2024-11-14 19:15:40 -0500

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,25 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
   },
 };
